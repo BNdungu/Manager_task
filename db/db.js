3a76e3aed3a930936e929ad7ffe19b7e066fdf17
BNdungu <bryannganga909@gmail.com>
2023-06-17 20:29:14 +0000

    _

--- db/db.js
+++ db/db.js
@@ -1,7 +1,5 @@
 const mongo = require('mongoose')
 
-//const url = 'mongodb+srv://ngangandungu:ndungu11@cluster0.6x2tkdu.mongodb.net/?retryWrites=true&w=majority'
-
 const connectDB = ((url) => {
     return mongo.connect(url,{
             useNewUrlParser : true,
